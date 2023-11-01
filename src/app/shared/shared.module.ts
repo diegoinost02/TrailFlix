2ef84a839786ec2b8180ebdf1ec515455629bd78
diegoinost02 <diegoinostroza00@gmail.com>
2023-10-31 21:21:02 -0300

    Estructura de carpetas

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ErrorComponent } from './error/error.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavBarComponent,
+    FooterComponent,
+    ErrorComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NavBarComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule { }
