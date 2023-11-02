0c820286c5f927a7ea6826afcace62d4c4a1f100
Juan Manuel Tretta Alvo <tretta.juanmanuel@gmail.com>
2023-11-02 18:05:24 -0300

    Home,navbar,footer
    Se comenzo el home, navbar y footer

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { PageComponent } from './components/page/page.component';
+import { MovieGridComponent } from './components/movie-grid/movie-grid.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageComponent,
+    MovieGridComponent
+  ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
