7a6b3fcc7579d4a2c0c010f8a3b9514316906998
diegoinost02 <diegoinostroza00@gmail.com>
2023-11-02 22:37:22 -0300

    Template de Login y Register completados

--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,5 +9,30 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent {
 
-  constructor(private router: Router){}
+  constructor(private router: Router, private formBuilder: FormBuilder){}
+
+  formUser = this.formBuilder.group({
+    'name': ['',Validators.required],
+    'email': ['', Validators.required], //,Validators.email],
+    'password': ['', Validators.required],
+    'passwordConfirmation': ['', Validators.required],
+    'conditions': ['', Validators.required]
+  })
+
+  get getName() {
+    return this.formUser.get('name') as FormControl;
+  }
+  get getEmail() {
+    return this.formUser.get('email') as FormControl;
+  }
+  get getPassword() {
+    return this.formUser.get('password') as FormControl;
+  }
+  get getPasswordConfirmation() {
+    return this.formUser.get('passwordConfirmation') as FormControl;
+  }
+  get getConditions() {
+    return this.formUser.get('conditions') as FormControl;
+  }
+
 }
