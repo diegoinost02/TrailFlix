aefb5ec4d770615e1afb351631f5b7672f493215
Juan Manuel Tretta Alvo <tretta.juanmanuel@gmail.com>
2023-11-05 00:04:00 -0300

    Home api y banner
    Se crearon interfaces,  se pudo conectar con la api para traer peliculas.

---
+++ src/app/core/services/movies.service.ts
@@ -0,0 +1,83 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map, of, tap } from 'rxjs';
+import { Movie, PeliculasResponse } from '../InterfaceMovies';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MoviesService {
+  private baseURL: string = 'https://api.themoviedb.org/3';
+  private PageNPlaying = 1;
+  private PagePopular = 1;
+
+  public cargando = false;
+  public cargandoPopular = false;
+
+  private apikey: string = '13ee2b3b1810d881d34a3d2f4351f448'; ///Buscar key nuestra
+  private language: string = 'es-ES';
+
+  constructor(private http: HttpClient) {}
+
+  get paramsNPlaying() {
+    return {
+      api_key: '13ee2b3b1810d881d34a3d2f4351f448',
+      language: 'es-ES',
+      page: this.PageNPlaying.toString(),
+    };
+  }
+  get paramsPopular() {
+    return {
+      api_key: '13ee2b3b1810d881d34a3d2f4351f448',
+      language: 'es-ES',
+      page: this.PagePopular.toString(),
+    };
+  }
+
+  
+
+  getPeliculas():Observable<Movie[]>{
+    console.log('cargando');
+    if (this.cargando) {
+     return of([]);
+    }
+       this.cargando=true;
+
+       return this.http.get<PeliculasResponse>(`${this.baseURL}/movie/now_playing`,{params:this.paramsNPlaying}).pipe(
+         map((res)=>res.results),
+         tap(()=>{
+           this.PageNPlaying+=1;
+           this.cargando=false;
+         })
+       );
+     }
+
+     getPeliculasTrendig():Observable<Movie[]>{
+      console.log('cargandoTrending');
+      if (this.cargandoPopular) {
+       return of([]);
+      }
+         this.cargandoPopular=true;
+  
+         return this.http.get<PeliculasResponse>(`${this.baseURL}/movie/popular`,{params:this.paramsNPlaying}).pipe(
+           map((res)=>res.results),
+           tap(()=>{
+             this.PagePopular+=1;
+             this.cargandoPopular=false;
+           })
+         );
+       }
+
+
+  
+
+  searchMovie() {}
+
+  getMovieDetail() {}
+
+  getCast() {}
+
+  getCategory() {}
+
+  resetPage() {}
+}
--- src/app/modules/home/components/movie-grid/movie-grid.component.ts
+++ src/app/modules/home/components/movie-grid/movie-grid.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/core/services/movies.service';
 
 @Component({
   selector: 'app-movie-grid',
   templateUrl: './movie-grid.component.html',
   styleUrls: ['./movie-grid.component.css']
 })
-export class MovieGridComponent {
+export class MovieGridComponent implements OnInit,AfterViewInit {
+
+  constructor(private movieSer:MoviesService) {}
+  ngAfterViewInit(): void {
+    
+  }
+ 
+  ngOnInit(): void {
+   
+  this.getMovies();
+  this.getBanner();
+  }
+
+  
+    movies:any = [];
+    banner:any = [];
+
+   
+  getMovies()
+  {
+    this.movieSer.getPeliculas().subscribe(movies=>{
+      this.movies = movies;
+      console.log(this.movies);
+    })
+  }
+
+  getBanner()
+  {
+    this.movieSer.getPeliculasTrendig().subscribe(movies=>{
+      this.banner = movies;
+      console.log(this.banner);
+    })
+  }
+
+
 
 }
