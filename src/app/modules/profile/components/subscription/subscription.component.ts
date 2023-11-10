5e0fa71791df4fd366a6da18448d38cf23b62924
diegoinost02 <diegoinostroza00@gmail.com>
2023-11-10 18:32:59 -0300

    Validación de contraseña para darse de baja

--- src/app/modules/profile/components/subscription/subscription.component.ts
+++ src/app/modules/profile/components/subscription/subscription.component.ts
@@ -1,8 +1,10 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-import { User } from 'src/app/core/Models';
+import { Popup, User } from 'src/app/core/Models';
 import { UsersService } from 'src/app/core/services/users.service';
+import { AlertPopupComponent } from 'src/app/shared/alert-popup/alert-popup.component';
 
 @Component({
   selector: 'app-subscription',
@@ -13,12 +15,21 @@ export class SubscriptionComponent {
 
   user: User = new User();
 
-  constructor(@Inject(MAT_DIALOG_DATA)public data: any, private dialogRef: MatDialogRef<SubscriptionComponent>, private usersService: UsersService, private router: Router) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialogRef<SubscriptionComponent>, private usersService: UsersService, private router: Router, private formBuilder: FormBuilder, private dialog: MatDialog) { }
+
+  formUser = this.formBuilder.group({
+    'password': ['', Validators.required]
+  })
+
+  dataAlert: Popup = {
+    title: 'No se pudo dar de baja',
+    body: 'La contraseña ingresada no coindice con tu contraseña actual'
+  }
 
   ngOnInit(): void {
     this.loadData();
   }
-  
+
   loadData() {
     const user = this.usersService.getCurrentUser();
     if (user) {
@@ -31,22 +42,48 @@ export class SubscriptionComponent {
   dialogData = {
     title: 'Plan',
     span: 'Subscripción Activa',
-    button: 'Dar de baja',
+    ocultVerification: true,
+    disguiseButtonVerify: false,
+    disguiseButtonSubmit: true,
     onClick: () => this.confirmation(),
   };
 
-  confirmation(){
+  confirmation() {
     this.dialogData = {
       title: 'Confirmar',
       span: '¿Estas seguro de darte de baja?',
-      button: 'Dar de baja',
-      onClick: () => this.deleteUser(),
+      ocultVerification: false,
+      disguiseButtonVerify: true,
+      disguiseButtonSubmit: false,
+      onClick: () => this.verifyPassword()
     };
   }
 
-  deleteUser(){
+  getPristine() {
+    return this.formUser.get('password') as FormControl;
+  }
+
+  verifyPassword() {
+    const password = this.formUser.value.password;
+    if (password) {
+      if (this.formUser.value.password === this.user.password) {
+        this.deleteUser();
+        this.dialogRef.close();
+      }
+      else {
+        const dialogRef = this.dialog.open(AlertPopupComponent, {
+          data: this.dataAlert, height: 'auto', width: '350px'
+        })
+        dialogRef.afterClosed().subscribe(result => {
+        })
+      }
+    }
+  }
+
+
+  deleteUser() {
     this.usersService.deleteUser(this.user.id!).subscribe({
-      next: () =>{
+      next: () => {
         this.router.navigate([''])
       },
       error: () => {
