5c508e67d4e863b391002a05950e9c343f939461
diegoinost02 <diegoinostroza00@gmail.com>
2023-11-11 12:09:05 -0300

    Arreglo importacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AboutUsComponent } from './standalones/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutUsComponent
   ],
   imports: [
     BrowserModule,
